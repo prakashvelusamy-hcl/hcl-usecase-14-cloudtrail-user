fd39ceb44a8db62da8e5545bc58d5aec94d01d37
Prakash_V <96634210+prakashvelusamy-hcl@users.noreply.github.com>
2025-06-23 15:10:02 +0530

    fix

--- modules/terraform-aws-cloudtrail/main.tf
+++ modules/terraform-aws-cloudtrail/main.tf
@@ -2,8 +2,8 @@ resource "aws_cloudtrail" "main" {
   name                          = var.cloud_trail_name
   s3_bucket_name                = var.cloud_trail_bucket_name
   include_global_service_events = true
-  is_logging                    = true
   is_multi_region_trail         = false
+  enable_log_file_validation    = true
 
   # Optional: You can enable logging for CloudWatch Logs
   cloud_watch_logs_group_arn    = "arn:aws:logs:us-west-2:123456789012:log-group:MyCloudWatchLogGroup"
