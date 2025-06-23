5af5b9abccdde363b2c899fe8d15d5ce92dc094b
Prakash_V <96634210+prakashvelusamy-hcl@users.noreply.github.com>
2025-06-23 15:03:01 +0530

    fix

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 module "s3" {
 source = "./modules/terraform-aws-s3"
-s3_bucket_name = var.s3_bucket_name
+cloud_trail_bucket_name = var.cloud_trail_bucket_name
 project_tag = var.project_tag
 }
 module "cloud_trail" {
 source = "./modules/terraform-aws-cloudtrail"
-s3_bucket_name =var.s3_bucket_name
+cloud_trail_bucket_name =var.cloud_trail_bucket_name
 project_tag = var.project_tag
 }
