6958f9eb51f68cde22f9262c0a061b2a7bab9302
Prakash_V <96634210+prakashvelusamy-hcl@users.noreply.github.com>
2025-06-23 15:26:38 +0530

    fix

---
+++ modules/terraform-aws-cloudtrail/iam.tf
@@ -0,0 +1,60 @@
+resource "aws_iam_role" "cloudtrail_role" {
+  name = "CloudTrailRole"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "cloudtrail.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+
+  tags = var.project_tag
+}
+
+resource "aws_iam_policy" "cloudtrail_policy" {
+  name        = "CloudTrailLoggingPolicy"
+  description = "Policy for CloudTrail to log to S3 and CloudWatch"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect   = "Allow"
+        Action   = [
+          "s3:PutObject",
+          "s3:ListBucket"
+        ]
+        Resource = [
+          "arn:aws:s3:::${var.cloud_trail_bucket_name}/*", 
+          "arn:aws:s3:::${var.cloud_trail_bucket_name}"
+        ]
+      },
+      {
+        Effect   = "Allow"
+        Action   = [
+          "logs:PutLogEvents",
+          "logs:CreateLogStream"
+        ]
+        Resource = [
+          "arn:aws:logs:us-west-2:123456789012:log-group:MyCloudWatchLogGroup:*" 
+        ]
+      },
+      {
+        Effect   = "Allow"
+        Action   = "cloudtrail:StartLogging"
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "cloudtrail_policy_attachment" {
+  policy_arn = aws_iam_policy.cloudtrail_policy.arn
+  role       = aws_iam_role.cloudtrail_role.name
+}
--- modules/terraform-aws-cloudtrail/main.tf
+++ modules/terraform-aws-cloudtrail/main.tf
@@ -7,7 +7,7 @@ resource "aws_cloudtrail" "main" {
 
   # Optional: You can enable logging for CloudWatch Logs
   cloud_watch_logs_group_arn    = "arn:aws:logs:us-west-2:123456789012:log-group:MyCloudWatchLogGroup"
-  cloud_watch_logs_role_arn     = "arn:aws:iam::123456789012:role/MyCloudWatchLogsRole"
+  cloud_watch_logs_role_arn     = "aws_iam_role.cloudtrail_role.arn"
 
   # Optional: Enable logging for S3 events
   event_selector {
