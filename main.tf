1ed69d827e207918c783ba0a233e448e2cf15f93
Prakash_V <96634210+prakashvelusamy-hcl@users.noreply.github.com>
2025-06-23 15:00:02 +0530

    fix

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
-modules "s3" {
+module "s3" {
 source = "./modules/terraform-aws-s3"
 s3_bucket_name = var.s3_bucket_name
 project_tag = var.project_tag
 }
-modules "cloud_trail" {
+module "cloud_trail" {
 source = "./modules/terraform-aws-cloud_trail"
 s3_bucket_name =var.s3_bucket_name
 project_tag = var.project_tag
--- variables.tf
+++ variables.tf
@@ -1,7 +1,7 @@
 variable "cloud_trail_bucket_name" {
 description = "The cloud_trail_bucket_name for storing the cloud Trail logs"
 type = string 
-
+}
 variable "project_tag" {
 description = "The project_tag for this project"
 type = map(string)
