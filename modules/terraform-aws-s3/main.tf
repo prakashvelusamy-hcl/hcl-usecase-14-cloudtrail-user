e71c17bca1ef95633120aabe84939f0c27edee24
Prakash_V <96634210+prakashvelusamy-hcl@users.noreply.github.com>
2025-06-23 15:17:01 +0530

    fix

--- modules/terraform-aws-s3/main.tf
+++ modules/terraform-aws-s3/main.tf
@@ -10,7 +10,7 @@ resource "aws_s3_bucket_versioning" "log_bucket_versioning" {
   }
 }
 
-resource "aws_s3_bucket_acl" "log_bucket_acl" {
-  bucket = aws_s3_bucket.log_bucket.bucket
-  acl    = "private" 
-}
+#resource "aws_s3_bucket_acl" "log_bucket_acl" {
+#  bucket = aws_s3_bucket.log_bucket.bucket
+#  acl    = "private" 
+#}
