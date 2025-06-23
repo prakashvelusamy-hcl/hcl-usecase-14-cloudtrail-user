ec7cc83b5e6a8319ba933d9a3ecab27ae5e0e298
Prakash_V <96634210+prakashvelusamy-hcl@users.noreply.github.com>
2025-06-23 15:36:14 +0530

    fix

--- modules/terraform-aws-cloudtrail/variables.tf
+++ modules/terraform-aws-cloudtrail/variables.tf
@@ -10,3 +10,7 @@ variable "cloud_trail_name" {
 description = "The Cloud Trail name" 
 type = string
 }
+variable "cloud_watch_logs_group_arn" {
+description = "The cloud watch log group arn"
+type = string
+}
