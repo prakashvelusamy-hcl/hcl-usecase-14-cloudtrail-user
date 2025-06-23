490bcdb020b8792de75e974dbcf0d86de64c7f2a
Prakash_V <96634210+prakashvelusamy-hcl@users.noreply.github.com>
2025-06-23 15:11:38 +0530

    fix

--- modules/terraform-aws-s3/main.tf
+++ modules/terraform-aws-s3/main.tf
@@ -1,9 +1,12 @@
 resource "aws_s3_bucket" "log_bucket" {
   bucket = var.cloud_trail_bucket_name
   acl    = "private"
-
-  versioning {
-    enabled = true
-  }
   tags  = var.project_tag
+}
+
+resource "aws_s3_bucket_versioning" "log_bucket_versioning" {
+  bucket = aws_s3_bucket.log_bucket.bucket
+  versioning_configuration {
+    status = "Enabled"
+  }
 }
