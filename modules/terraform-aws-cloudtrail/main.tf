389ed20c119c566ff2352bb4df3d177c7d947c72
Prakash_V <96634210+prakashvelusamy-hcl@users.noreply.github.com>
2025-06-24 11:16:28 +0530

    multi_region_trail

--- modules/terraform-aws-cloudtrail/main.tf
+++ modules/terraform-aws-cloudtrail/main.tf
@@ -2,7 +2,7 @@ resource "aws_cloudtrail" "main" {
   name                          = var.cloud_trail_name
   s3_bucket_name                = var.cloud_trail_bucket_name
   include_global_service_events = true
-  is_multi_region_trail         = false
+  is_multi_region_trail         = true
   enable_log_file_validation    = true
 
   cloud_watch_logs_group_arn    = "${var.cloud_watch_logs_group_arn}:*"
