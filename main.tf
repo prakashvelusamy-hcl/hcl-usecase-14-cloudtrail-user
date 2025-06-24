91b3204a4fbe5a5baeffe634db87c04e993a057d
Prakash_V <96634210+prakashvelusamy-hcl@users.noreply.github.com>
2025-06-24 09:51:06 +0530

    fix

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ cloud_trail_bucket_name =var.cloud_trail_bucket_name
 cloud_trail_name = var.cloud_trail_name
 project_tag = var.project_tag
 cloud_watch_logs_group_arn = module.cloud_watch.cloud_watch_logs_group_arn
- depends_on = [module.cloud_watch]
+ depends_on = [module.cloud_watch , module.s3]
  aws_region = var.aws_region
 }
 
